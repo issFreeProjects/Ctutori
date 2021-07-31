d047dc83513478be316515f219e4e72a298aef2c
iss <iss.siamak@gmail.com>
2021-07-31 17:37:17 +0430

    Fibonacci (fibo) added to mmath

--- utils/mmath.h
+++ utils/mmath.h
@@ -6,3 +6,6 @@ int mabs(int n);
 
 // find lcm of two integers lcm(a,b) = |ab| / gcd(a,b)
 int mlcm(int a, int b);
+
+// Fibonacci Series fibo(0)=fibo(1)=1 and for n>1 => fibo(n)=fibo(n-1)+fibo(n-2)
+int fibo(int n);
