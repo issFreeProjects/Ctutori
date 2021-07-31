5b6526d8b9932e58c6114f41062e44df8caa062c
iss <iss.siamak@gmail.com>
2021-07-31 17:02:48 +0430

    abs changed to mabs and findabs use mabs

--- utils/mmath.c
+++ utils/mmath.c
@@ -1,5 +1,14 @@
+int mabs(int n)
+{
+    if( n>0 )
+        return n;
+    else return n*(-1);
+}
+
+
 int findGCD(int a,int b)
 {
+    a = mabs(a);  b=mabs(b);
      while(a!=b)
      {
           if(a>b)
@@ -8,12 +17,4 @@ int findGCD(int a,int b)
              return findGCD(a,b-a);
      }
      return a;
-}
-
-
-int abs(int n)
-{
-    if( n>0 )
-        return n;
-    else return n*(-1);
 }
--- utils/mmath.h
+++ utils/mmath.h
@@ -2,4 +2,4 @@
 int findGCD(int a,int b);
 
 // find abs of integers abs(x) = (x if x>0) (-x is x<0)
-int abs(int n);
+int mabs(int n);
