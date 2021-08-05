b3431168d8e894ca8a06254aa1bd3df4d1ed0b44
iss <iss.siamak@gmail.com>
2021-08-05 11:39:45 +0430

    add print method

--- ds/binarySearchTree.c
+++ ds/binarySearchTree.c
@@ -1,1 +1,83 @@
 #include <binarySearchTree.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+bsTree* initBSTree(int headData)
+{
+    bsTree* head = (bsTree*)malloc(sizeof(bsTree*));
+    head->data = headData;
+    return head;
+}
+
+
+// internal method
+int _print_t(bsTree *tree, int is_left, int offset, int depth, char s[20][255])
+{
+    char b[20];
+    int width = 5;
+
+    if (!tree) return 0;
+
+    sprintf(b, "(%03d)", tree->data);
+
+    int left  = _print_t(tree->lChild,  1, offset,                depth + 1, s);
+    int right = _print_t(tree->rChild, 0, offset + left + width, depth + 1, s);
+
+#ifdef COMPACT
+    for (int i = 0; i < width; i++)
+        s[depth][offset + left + i] = b[i];
+
+    if (depth && is_left) {
+
+        for (int i = 0; i < width + right; i++)
+            s[depth - 1][offset + left + width/2 + i] = '-';
+
+        s[depth - 1][offset + left + width/2] = '.';
+
+    } else if (depth && !is_left) {
+
+        for (int i = 0; i < left + width; i++)
+            s[depth - 1][offset - width/2 + i] = '-';
+
+        s[depth - 1][offset + left + width/2] = '.';
+    }
+#else
+    for (int i = 0; i < width; i++)
+        s[2 * depth][offset + left + i] = b[i];
+
+    if (depth && is_left) {
+
+        for (int i = 0; i < width + right; i++)
+            s[2 * depth - 1][offset + left + width/2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width/2] = '+';
+
+    } else if (depth && !is_left) {
+
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width/2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset - width/2 - 1] = '+';
+    }
+#endif
+
+    return left + width + right;
+}
+
+
+// print bst
+void bstPrint(bsTree *head)
+{
+    char s[20][255];
+    for (int i = 0; i < 20; i++)
+        sprintf(s[i], "%80s", " ");
+
+    _print_t(head, 0, 0, 0, s);
+
+    for (int i = 0; i < 20; i++)
+        printf("%s\n", s[i]);
+}
--- ds/binarySearchTree.h
+++ ds/binarySearchTree.h
@@ -1,1 +1,28 @@
 #include "bstree.h"
+
+/**
+ *      This is a simple implementation of binary search tree
+ * 
+ *                          *  *
+ *                       *        *                  
+ *                      *  data=A  *   
+ *                      *          *
+ *                       *        * 
+ *                        / *  * \
+ *                       /        \
+ *                      /          \
+ *                 *  *              *  *
+ *              *        *        *        *
+ *             *  data<A  *      *  data>A  *
+ *             *          *      *          *
+ *              *        *        *        *
+ *                 *  *              *  *
+ */
+
+
+// some initialize, headData is value of head node
+bsTree* initBSTree(int headData);
+
+
+// use it for print a tree
+void bstPrint(bsTree *head);
